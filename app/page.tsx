7889f4d71a06b3803a17478a626093f23eca84f1
Rohit <rohityadav787898@gmail.com>
2024-07-06 22:27:08 +0545

    added public folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
-  return <div>Careplus</div>;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            height={1000}
+            width={1000}
+            className="mb-12 h-10 w-fit"
+          />
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
